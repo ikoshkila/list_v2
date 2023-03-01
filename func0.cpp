b22f2e5b4ae0cc27cbe634760ba36d4af84324d1
ikoshkila <114185021+ikoshkila@users.noreply.github.com>
2023-03-01 11:58:05 -0900

    Update func0.cpp

--- func0.cpp
+++ func0.cpp
@@ -2,7 +2,7 @@
 
 std::vector<char> itc_strlist(std::string str){
     std::vector<char> o;
-    for(int i=0; str[i] != '\0'; i++){
+    for(long long i=0; str[i] != '\0'; i++){
         o.push_back(str[i]);
     }
     return o;
@@ -10,7 +10,7 @@ std::vector<char> itc_strlist(std::string str){
 
 std::string itc_join(std::vector<char> lst, std::string sep){
     std::string o;
-    for(int i=0; i<lst.size(); i++){
+    for(long long i=0; i<lst.size(); i++){
         o += lst[i] + sep;
     }
     return o;
@@ -18,7 +18,7 @@ std::string itc_join(std::vector<char> lst, std::string sep){
 
 std::string itc_rmstrspc(std::string str){
     std::string o;
-    for(int i=0; str[i] != '\0'; i++){
+    for(long long i=0; str[i] != '\0'; i++){
         if(str[i] != ' '){
             o += str[i];
         }
@@ -29,9 +29,9 @@ std::string itc_rmstrspc(std::string str){
 std::string itc_rmstrchar(std::string str, std::string less){
     std::string o;
     bool check = 1;
-    for(int i=0; str[i] != '\0'; i++){
+    for(long long i=0; str[i] != '\0'; i++){
         check = 1;
-        for(int j=0; less[j] != '\0'; j++){
+        for(long long j=0; less[j] != '\0'; j++){
             if(str[i] == less[j]){
                 check = 0;
                 break;
@@ -46,7 +46,7 @@ std::string itc_rmstrchar(std::string str, std::string less){
 
 long itc_sumlst(const std::vector<int> &lst){
     long o = 0;
-    for(int i=0; i<lst.size(); i++){
+    for(long long i=0; i<lst.size(); i++){
         o += lst[i];
     }
     return o;
